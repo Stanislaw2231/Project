37756b768111a8e822a58e75c31577fd8ac34d78
Stanislaw <s24645@pjwstk.edu.pl>
2025-01-25 23:23:46 +0100

    asdf

--- src/suml/pipelines/app/streamlit.py
+++ src/suml/pipelines/app/streamlit.py
@@ -14,22 +14,6 @@ MODEL_PATH = "data/models"
 predictor = TabularPredictor.load(MODEL_PATH)
 
 
-# Load the dataset
-@st.cache_data
-def load_data():
-    """
-    Load the data from a specified CSV file and return it as a pandas DataFrame.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing the data from the CSV file.
-    """
-
-    return pd.read_csv("data/raw/Food_Time_Data_Set.csv")
-
-
-data = load_data()
-
-
 def main():
     """
     Displays a Streamlit application for delivery time prediction.
@@ -48,58 +32,45 @@ def main():
     input_data_placeholder = st.empty()
 
     col1, col2, col3, col4 = st.columns([1, 1, 0.5, 1])
-
     user_input = {}
 
     with col1:
         user_input["precipitation"] = st.slider(
-            "Precipitation",
-            float(data["precipitation"].dropna().min()),
-            float(data["precipitation"].dropna().max()),
-            step=0.1,
-            key="precipitation",
+            "Precipitation", 0.0, 1.46, step=0.1, key="precipitation"
         )
         user_input["Restaurant_latitude"] = st.slider(
             "Restaurant Latitude",
-            float(data["Restaurant_latitude"].dropna().min()),
-            float(data["Restaurant_latitude"].dropna().max()),
+            -30.902872,
+            30.914057,
             step=0.001,
             key="restaurant_latitude",
         )
         user_input["Delivery_location_latitude"] = st.slider(
             "Delivery Location Latitude",
-            float(data["Delivery_location_latitude"].dropna().min()),
-            float(data["Delivery_location_latitude"].dropna().max()),
+            0.01,
+            31.054057,
             step=0.001,
             key="delivery_location_latitude",
         )
         user_input["Distance (km)"] = st.slider(
-            "Distance",
-            float(data["Distance (km)"].dropna().min()),
-            float(data["Distance (km)"].dropna().max()),
-            step=1.0,
-            key="distance",
+            "Distance", 1.55, 59.84, step=1.0, key="distance"
         )
 
     with col2:
         user_input["Delivery_person_Age"] = st.slider(
-            "Delivery Person Age",
-            int(data["Delivery_person_Age"].dropna().min()),
-            int(data["Delivery_person_Age"].dropna().max()),
-            step=1,
-            key="delivery_person_age",
+            "Delivery Person Age", 15, 50, step=1, key="delivery_person_age"
         )
         user_input["Restaurant_longitude"] = st.slider(
             "Restaurant Longitude",
-            float(data["Restaurant_longitude"].dropna().min()),
-            float(data["Restaurant_longitude"].dropna().max()),
+            -88.352885,
+            88.433452,
             step=0.001,
             key="restaurant_longitude",
         )
         user_input["Delivery_location_longitude"] = st.slider(
             "Delivery Location Longitude",
-            float(data["Delivery_location_longitude"].dropna().min()),
-            float(data["Delivery_location_longitude"].dropna().max()),
+            0.01,
+            88.563452,
             step=0.001,
             key="delivery_location_longitude",
         )
@@ -110,29 +81,48 @@ def main():
     with col3:
         user_input["Type_of_order"] = st.radio(
             "Type of Order",
-            data["Type_of_order"].dropna().unique(),
+            ["Snack ", "Drinks ", "Buffet ", "Meal "],
             key="type_of_order",
         )
         user_input["Type_of_vehicle"] = st.radio(
             "Type of Vehicle",
-            data["Type_of_vehicle"].dropna().unique(),
+            ["motorcycle ", "scooter ", "electric_scooter ", "bicycle "],
             key="type_of_vehicle",
         )
         user_input["Traffic_Level"] = st.radio(
-            "Traffic Level",
-            ["Very Low", "Low", "Moderate", "High", "Very High"],
-            key="traffic_level",
+            "Traffic Level", ["Very Low", "Low", "Moderate", "High", "Very High"], key="traffic_level"
         )
 
     with col4:
         user_input["weather_description"] = st.selectbox(
             "Weather Description",
-            data["weather_description"].dropna().unique(),
+            [
+                "haze",
+                "mist",
+                "broken clouds",
+                "clear sky",
+                "scattered clouds",
+                "overcast clouds",
+                "light rain",
+                "smoke",
+                "fog",
+                "few clouds",
+                "moderate rain",
+            ],
             key="weather_description",
         )
-        delivery_person_ids = [""] + list(data["Delivery_person_ID"].dropna().unique())
         user_input["Delivery_person_ID"] = st.selectbox(
-            "Delivery Person ID", delivery_person_ids, key="delivery_person_id"
+            "Delivery Person ID",
+            [
+                "",
+                "INDORES13DEL02",
+                "BANGRES18DEL02",
+                "BANGRES19DEL01",
+                "BHPRES11DEL01",
+                "BHPRES09DEL02",
+                "ALHRES13DEL01",
+            ],
+            key="delivery_person_id",
         )
         temp_input = st.text_input("Temperature (°C)", value="0", key="temperature")
         try:
